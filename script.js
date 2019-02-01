0f6af259c88d7557461c14c59c11f2c01b517bff
Dirosaki <diegosgomes27@gmail.com>
2019-02-01 14:05:39 -0200

    Responsividade e adição de buttons

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+let body = document.querySelector('body');
 let listElement = document.querySelector('ul');
 let inputElement = document.querySelector('input');
-let buttonElement = document.querySelector('button');
+let buttonElement = document.querySelector('.button-disable');
+let buttonElement1 = document.querySelector('.button1-disable');
+let buttonElement2 = document.querySelector('.button2-disable');
 
 let todos = JSON.parse(localStorage.getItem('list_todos')) || ['Diego'];
+
+setInterval(function(){
+    if(inputElement.value.trim() != ""){
+        buttonElement.classList.add('button-active');
+        buttonElement.classList.remove('button-disable');
+        buttonElement.disabled = false;
+    }else{
+        buttonElement.classList.remove('button-active');
+        buttonElement.classList.add('button-disable');
+        buttonElement.disabled = true;
+    }
+}, 100);
+
+setInterval(() => {
+    if(listElement.innerHTML.length != 0){
+        buttonElement2.classList.add('button2-active');
+        buttonElement2.classList.remove('button2-disable');
+        buttonElement2.disabled = false;
+    }else{
+        buttonElement2.classList.remove('button2-active');
+        buttonElement2.classList.add('button2-disable');
+        buttonElement2.disabled = true;
+    }
+}, 200)
 
 function renderTodos(){
     listElement.innerHTML = '';
@@ -11,52 +38,112 @@ function renderTodos(){
         let todoText = document.createTextNode(todo);
 
         let linkElement = document.createElement('a');
+        let spanElement = document.createElement('span');
 
         linkElement.setAttribute('href', '#');
 
         let pos = todos.indexOf(todo);
         linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
 
-        let linkText = document.createTextNode('Excluir');
+        // let linkText = document.createTextNode('Excluir');
+        let lixeira = document.createElement('i');
+        let confirm = document.createElement('i');
+        // let spanText = document.createTextNode('Concluir');
 
-        linkElement.appendChild(linkText);
+
+        // linkElement.appendChild(linkText);
+        // spanElement.appendChild(spanText);
 
         todoElement.appendChild(todoText);
         todoElement.appendChild(linkElement);
-
+        todoElement.appendChild(spanElement);
+        linkElement.appendChild(lixeira);
+        spanElement.appendChild(confirm);
         listElement.appendChild(todoElement);
     }
 }
+
+
+
+function teste(){
+    if(listElement.innerHTML != ""){
+        let confirms = document.querySelectorAll('span>i');
+        let lixos = document.querySelectorAll('a>i');
+        let tarefas = document.querySelectorAll('li');
+
+        for(let tarefa of tarefas){
+            tarefa.onclick = () => {
+                tarefa.classList.toggle('confirmation');
+                
+                // return;
+            }
+            buttonElement1.onclick = () => {
+            for(tarefa of tarefas){
+                    tarefa.classList.toggle('confirmation');
+                }
+            }
+            
+        }
+
+        buttonElement1.classList.add('button1-active');
+        buttonElement1.classList.remove('button1-disable');
+
+        for(let confirm of confirms){
+            confirm.classList.add('fas');
+            confirm.classList.add('fa-check'); 
+        }
+
+        for(let lixo of lixos){
+            lixo.classList.add('fas');    
+            lixo.classList.add('fa-trash-alt');    
+        }
+        
+    }else{
+        buttonElement1.classList.remove('button1-active');
+        buttonElement1.classList.add('button1-disable');
+    }
+    return;
+}
+
+
+setInterval(teste, 200);
 
 renderTodos();
 
 function addTodo(){
     let todoText = inputElement.value;
+    
     if(todoText.trim()){
         todos.push(todoText);
     inputElement.value = '';
+    
     renderTodos();
     saveToStorage();
-    }
-    else{
+    // teste();
+    }else{
         alert('Campo em Branco');
-    }
-
-    
+    }    
 }
 
-buttonElement.onclick = function(){
-    addTodo();
-}
+onkeyup = (evento) => (evento.key == "Enter") ? addTodo() : 0;
 
-function deleteTodo(pos){
+buttonElement.onclick = () => addTodo();
+
+
+let deleteTodo = (pos) => {
     todos.splice(pos, 1);
     renderTodos();
     saveToStorage();
 }
 
-function saveToStorage(){
-
-    localStorage.setItem('list_todos', JSON.stringify(todos));
-
+buttonElement2.onclick = () => {
+    deleteTodo2();
 }
+
+let deleteTodo2 = (pos) => {
+    todos.splice(pos);
+    renderTodos();
+    saveToStorage();
+}
+
+let saveToStorage = () => localStorage.setItem('list_todos', JSON.stringify(todos));
